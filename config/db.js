7d3b94f41d87c6de0a447b516ecdb356880d766a
danielrangel01 <danielrangel2417@gmail.com>
2025-06-27 05:02:44 -0500

    edit dotenvconf2

--- config/db.js
+++ config/db.js
@@ -6,7 +6,7 @@ dotenv.config();
 
 const connectDB = async () => {
   try {
-    await mongoose.connect(process.env.MONGO_URI);
+    await mongoose.connect(process.env.MONGODB_URI);
     console.log('MongoDB conectado');
   } catch (error) {
     console.error('Error al conectar a MongoDB:', error);
