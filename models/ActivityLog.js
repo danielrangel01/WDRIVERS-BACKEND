b10095ae0fbb557f1ae4ec09a34677ffda317192
danielrangel01 <danielrangel2417@gmail.com>
2025-07-08 07:16:06 -0500

    add new item enum list

--- models/ActivityLog.js
+++ models/ActivityLog.js
@@ -1,11 +1,29 @@
-
-import mongoose from 'mongoose';
+import mongoose from "mongoose";
 
 const activitySchema = new mongoose.Schema({
-  usuario: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
-  tipo: { type: String, enum: ['pago', 'gasto', 'usuario', 'login', 'aprobacion pago', 'Creacion de usuario', 'pago deuda', 'Deuda generada', ], required: true },
+  usuario: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "User",
+    required: true,
+  },
+  tipo: {
+    type: String,
+    enum: [
+      "pago",
+      "gasto",
+      "usuario",
+      "login",
+      "aprobacion pago",
+      "Creacion de usuario",
+      "pago deuda",
+      "Deuda generada",
+      "eliminacion deuda",
+      "editar deuda",
+    ],
+    required: true,
+  },
   descripcion: { type: String, required: true },
-  fecha: { type: Date, default: Date.now }
+  fecha: { type: Date, default: Date.now },
 });
 
-export default mongoose.model('ActivityLog', activitySchema);
+export default mongoose.model("ActivityLog", activitySchema);
